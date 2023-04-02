d39372c47650ecb102e796fb1aa45176a017220c
Sudeep Khandekar <Sudeep.Khandekar@ril.com>
2023-04-02 15:27:18 +0530

    shifting directories

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ strategy = st.selectbox('Select the trading strategy:', options=['SMA', 'EMA', '
 
 if st.button('Run Backtest'):
     # Read data for the given stock symbol and date range
-    data = pd.read_csv(f"/PythonForFinance/13. Data Collection/Data/{symbol}.csv", parse_dates=['Date'], index_col='Date')
+    data = pd.read_csv(f"/{symbol}.csv", parse_dates=['Date'], index_col='Date')
 
     data = data.loc[start_date:end_date]
 
